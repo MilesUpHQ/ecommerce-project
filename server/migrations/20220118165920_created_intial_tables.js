479a2c606819195572da382f4eeef72a6aa23eb2
manisaiprasadam <manisaiprasadam@gmail.com>
2022-01-19 13:54:27 +0530

    Updated tables based on feedback

--- server/migrations/20220118165920_created_intial_tables.js
+++ server/migrations/20220118165920_created_intial_tables.js
@@ -2,7 +2,7 @@
 exports.up = function(knex) {
     return knex.schema
     .createTable('users', (table) => {
-        table.increments('user_id').primary();
+        table.increments('id').primary();
         table.string('username').unique().notNullable();
         table.string('password_digest').notNullable();
         table.string('email').unique().notNullable();
@@ -12,135 +12,151 @@ exports.up = function(knex) {
         table.string('avatar_url');
         table.timestamps(true, true);
     })
-    .createTable('ProductCategories', (table) => {
-        table.increments('categories_id').primary();
-        table.string('name').notNullable();
-        table.string('description');
-        table.timestamps(true, true);
-    })
-    .createTable('SubCategory',(table) => {
-        table.increments('sub_category_id').primary();
+    .createTable('product_categories', (table) => {
+        table.increments('id').primary();
         table.string('name').notNullable();
         table.string('description');
         table.integer('parent_id').unsigned().notNullable();
-        table.foreign('parent_id').references('ProductCategories.categories_id');
-        table.timestamps(true, true);   
+        table.foreign('parent_id').references('product_categories.id');
+        table.timestamps(true, true);
     })
-    .createTable('Products', (table) => {
-        table.increments('products_id').primary();
+    .createTable('products', (table) => {
+        table.increments('id').primary();
         table.string('name').notNullable();
         table.string('description');
-        table.string('image_url');
         table.integer('price').notNullable();
         table.integer('category_id').unsigned().notNullable();
-        table.foreign('category_id').references('ProductCategories.categories_id');
-        table.integer('sub_category_id').unsigned().notNullable();
-        table.foreign('sub_category_id').references('SubCategory.sub_category_id');
+        table.foreign('category_id').references('product_categories.id');
         table.timestamps(true, true);
     })
-    .createTable('Orders', (table) => {
-        table.increments('orders_id').primary();
-        table.dateTime('order_date').notNullable();
-        table.integer('total_price').notNullable();
-        table.string('status').notNullable();
-        table.integer('user_id').unsigned().notNullable();
-        table.foreign('user_id').references('users.user_id');
-        table.timestamps(true, true);
-    })
-    .createTable('OrderItems', (table) => {
-        table.increments('order_items_id').primary();
-        table.integer('quantity').notNullable();
+    .createTable('product_images', (table) => {
+        table.increments('id').primary();
+        table.string('image_url').notNullable();
         table.integer('product_id').unsigned().notNullable();
-        table.foreign('product_id').references('Products.products_id');
-        table.integer('order_id').unsigned().notNullable();
-        table.foreign('order_id').references('Orders.orders_id');
+        table.foreign('product_id').references('products.id');
         table.timestamps(true, true);
     })
-    .createTable('Cart', (table) => {
-        table.increments('cart_id').primary();
-        table.integer('quantity').notNullable();
-        table.integer('product_id').unsigned().notNullable();
-        table.foreign('product_id').references('Products.products_id');
-        table.integer('user_id').unsigned().notNullable();
-        table.foreign('user_id').references('users.user_id');
+    .createTable('country', (table) => {
+        table.increments('id').primary();
+        table.string('name').notNullable();
+        table.string('code').notNullable();
         table.timestamps(true, true);
     })
-    .createTable('Wishlist',(table) => {
-        table.increments('wishlist_id').primary();
-        table.integer('user_id').unsigned().notNullable();
-        table.foreign('user_id').references('users.user_id');
-        table.integer('product_id').unsigned().notNullable();
-        table.foreign('product_id').references('Products.products_id');
-        table.timestamps(true, true);
-    })
-    .createTable('Reviews', (table) => {
-        table.increments('reviews_id').primary();
-        table.integer('rating').notNullable();
-        table.string('comment').notNullable();
-        table.integer('product_id').unsigned().notNullable();
-        table.foreign('product_id').references('Products.products_id');
-        table.integer('user_id').unsigned().notNullable();
-        table.foreign('user_id').references('users.user_id');
-        table.timestamps(true, true);
-    })
-    .createTable('Payment',(table) => {
-        table.increments('payment_id').primary();
-        table.string('type').notNullable();
-        table.string('Status').notNullable();
-        table.integer('order_id').unsigned().notNullable();
-        table.foreign('order_id').references('Orders.orders_id');
-        table.timestamps(true, true);
-    })
-    .createTable('Shipping',(table) => {
-        table.increments('shipping_id').primary();
-        table.string('type').notNullable();
-        table.string('status').notNullable();
-        table.integer('order_id').unsigned().notNullable();
-        table.foreign('order_id').references('Orders.orders_id');
-        table.timestamps(true, true);
-    })
-    .createTable('FeturedProducts',(table) => {
-        table.increments('featured_id').primary();
-        table.integer('product_id').unsigned().notNullable();
-        table.foreign('product_id').references('Products.products_id');
-        table.timestamps(true, true);
-    })
-    .createTable('Address',(table) => {
-        table.increments('address_id').primary();
+    .createTable('address', (table) => {
+        table.increments('id').primary();
         table.string('street').notNullable();
         table.string('city').notNullable();
         table.string('state').notNullable();
-        table.string('pin').notNullable();
-        table.string('country').notNullable();
+        table.string('pin_code').notNullable();
+        table.integer('country_id').unsigned().notNullable();
+        table.foreign('country_id').references('country.id');
         table.integer('user_id').unsigned().notNullable();
-        table.foreign('user_id').references('users.user_id');
+        table.foreign('user_id').references('users.id');
         table.timestamps(true, true);
     })
-    .createTable('Varients',(table) => {
-        table.increments('varients_id').primary();
-        table.string('size').notNullable();
-        table.string('color').notNullable();
-        table.string('type').notNullable();
+    .createTable('orders', (table) => {
+        table.increments('id').primary();
+        table.dateTime('order_date').notNullable();
+        table.integer('total_price').notNullable();
+        table.string('status').notNullable();
+        table.integer('address_id').unsigned().notNullable();
+        table.foreign('address_id').references('address.id');
+        table.integer('user_id').unsigned().notNullable();
+        table.foreign('user_id').references('users.id');
+        table.timestamps(true, true);
+    })
+    .createTable('order_items', (table) => {
+        table.increments('id').primary();
+        table.integer('quantity').notNullable();
         table.integer('product_id').unsigned().notNullable();
-        table.foreign('product_id').references('Products.products_id');
+        table.foreign('product_id').references('products.id');
+        table.integer('order_id').unsigned().notNullable();
+        table.foreign('order_id').references('orders.id');
+        table.timestamps(true, true);
+    })
+    .createTable('cart', (table) => {
+        table.increments('id').primary();
+        table.integer('price').notNullable();
+        table.integer('user_id').unsigned().notNullable();
+        table.foreign('user_id').references('users.id');
+        table.timestamps(true, true);
+    })
+    .createTable('cart_items', (table) => {
+        table.increments('id').primary();
+        table.integer('quantity').notNullable();
+        table.integer('product_id').unsigned().notNullable();
+        table.foreign('product_id').references('products.id');
+        table.integer('cart_id').unsigned().notNullable();
+        table.foreign('cart_id').references('cart.id');
+        table.timestamps(true, true);
+    })
+    .createTable('wishlist',(table) => {
+        table.increments('id').primary();
+        table.integer('user_id').unsigned().notNullable();
+        table.foreign('user_id').references('users.id');
+        table.integer('product_id').unsigned().notNullable();
+        table.foreign('product_id').references('products.id');
+        table.timestamps(true, true);
+    })
+    .createTable('reviews', (table) => {
+        table.increments('id').primary();
+        table.integer('rating').notNullable();
+        table.string('comment').notNullable();
+        table.integer('product_id').unsigned().notNullable();
+        table.foreign('product_id').references('products.id');
+        table.integer('user_id').unsigned().notNullable();
+        table.foreign('user_id').references('users.id');
+        table.timestamps(true, true);
+    })
+    .createTable('payment',(table) => {
+        table.increments('id').primary();
+        table.string('type').notNullable();
+        table.string('status').notNullable();
+        table.integer('order_id').unsigned().notNullable();
+        table.foreign('order_id').references('orders.id');
+        table.timestamps(true, true);
+    })
+    .createTable('shipping',(table) => {
+        table.increments('id').primary();
+        table.string('type').notNullable();
+        table.string('status').notNullable();
+        table.integer('order_id').unsigned().notNullable();
+        table.foreign('order_id').references('orders.id');
+        table.timestamps(true, true);
+    })
+    .createTable('fetured_products',(table) => {
+        table.increments('id').primary();
+        table.integer('product_id').unsigned().notNullable();
+        table.foreign('product_id').references('products.id');
+        table.timestamps(true, true);
+    })
+    .createTable('variants',(table) => {
+        table.increments('id').primary();
+        table.string('size')
+        table.string('color')
+        table.string('type')
+        table.integer('product_id').unsigned().notNullable();
+        table.foreign('product_id').references('products.id');
         table.timestamps(true, true);
     });
 };
 
 exports.down = function(knex) {
     return knex.schema
-    .dropTable('Varients')
-    .dropTable('Address')
-    .dropTable('FeturedProducts')
-    .dropTable('Shipping')
-    .dropTable('Payment')
-    .dropTable('Reviews')
-    .dropTable('Wishlist')
-    .dropTable('Cart')
-    .dropTable('OrderItems')
-    .dropTable('Orders')
-    .dropTable('Products')
-    .dropTable('SubCategory')
-    .dropTable('ProductCategories')
+    .dropTable('variants')
+    .dropTable('fetured_products')
+    .dropTable('shipping')
+    .dropTable('payment')
+    .dropTable('reviews')
+    .dropTable('wishlist')
+    .dropTable('cart_items')
+    .dropTable('cart')
+    .dropTable('order_items')
+    .dropTable('orders')
+    .dropTable('address')
+    .dropTable('country')
+    .dropTable('product_images')
+    .dropTable('products')
+    .dropTable('product_categories')
     .dropTable('users');
 };
