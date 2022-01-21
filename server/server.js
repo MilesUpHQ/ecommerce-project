55a1ec62690e77aad7d129b8a3571d43ce1758b7
manisaiprasadam <manisaiprasadam@gmail.com>
2022-01-21 06:10:16 +0530

    created sign up and jwt login routes and handlers

--- server/server.js
+++ server/server.js
@@ -4,14 +4,44 @@ const dotenv = require("dotenv");
 const port = 4000;
 dotenv.config();
 
+const passport = require('passport');
+const passportJWT = require('passport-jwt');
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
 const environment = process.env.NODE_ENV || "development";
 const config = require("./knexfile")[environment];
 const knex = require("knex")(config);
+const bookshelf = require('bookshelf')(knex);
+const securePassword = require('bookshelf-secure-password');
+bookshelf.plugin(securePassword);
+const jwt = require('jsonwebtoken');
 const cors = require("cors");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+const User = bookshelf.Model.extend({
+  tableName: 'users',
+  hasSecurePassword: true
+});
+
+const opts = {
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET
+};
+
+const strategy = new JWTStrategy(opts, (jwt_payload, next) => {
+  console.log('payload received', jwt_payload);
+  User.forge({ id: jwt_payload.id }).fetch().then(res => {
+      next(null, res);
+  })
+});
+
+use(bodyParser.json());
+passport.use(strategy);
+app.use(passport.initialize());
+app.
 
 app.get("/", (req, res) => {
   res.json({ name: "Magesh", company: "Sedin pvt" });
@@ -21,6 +51,46 @@ app.listen(port, () =>
   console.log(`JS Bootcamp project listening on port ${port}!`)
 );
 
+
+app.post('/signup',(req,res)=>{
+  console.log(req);
+  console.log(req.body);
+
+  if (!req.body.username || !req.body.password || !req.body.email || !req.body.first_name || !req.body.last_name) {
+      res.status(400).send('Please provide the details');
+  }
+  const user = new User({
+      email: req.body.email,
+      user_name: req.body.username,
+      password: req.body.password,
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+  });
+  console.log(user);
+  user.save().then(() => {
+      res.send('User created successfully');
+  });
+})
+
+
+app.post('/getToken', (req, res) => {
+  console.log(req.body);
+  console.log(req.body.email);
+  console.log(req.body.password);
+  if(!req.body.email || !req.body.password){
+      res.status(401).send('Please provide email and password');
+  }
+  User.forge({ email: req.body.email }).fetch().then(user => {
+      user.authenticate(req.body.password).then(() => {
+          const payload = { id: user.id };
+          const token = jwt.sign(payload, process.env.JWT_SECRET);
+          res.json({ token });
+      }).catch(err => {
+          res.status(401).send('Invalid email or password');
+      });
+  });
+
+});
 
 // /admin/products
 //   / admin/products/new
