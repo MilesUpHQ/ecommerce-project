36ce27e0f98f8dafc478cd99b71601f2976cec11
manisaiprasadam <manisaiprasadam@gmail.com>
2022-01-25 11:21:21 +0530

    added react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes, Navigate} from 'react-router-dom';
+import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
+import Home from './components/Home/Home';
+import Logout from './components/Logout/Logout';
+import {getJWT} from './utils/jwt';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="/logout" element={<Logout />} />
+
+        <Route exact path="/" element={<PrivateRoute>
+          <Home />
+        </PrivateRoute>}>
+        </Route>   
+
+      </Routes>
+    </BrowserRouter>
   );
+}
+
+function PrivateRoute({ children }) {
+  const auth = useAuth();
+  return auth ? children : <Navigate to="/login" />;
+}
+function useAuth() {
+  const jwt = getJWT();
+  return jwt && jwt !== 'undefined' && jwt !== 'null';
 }
 
 export default App;
