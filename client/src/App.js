524494b0f52f3b031b372e2e1cf80f9274d143a9
manisaiprasadam <manisaiprasadam@gmail.com>
2022-01-27 06:36:51 +0530

    Formated neatly

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,68 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Login from './components/Login/Login';
-import Signup from './components/Signup/Signup';
-import Home from './components/Home/Home';
-import Logout from './components/Logout/Logout';
-import {getJWT} from './utils/jwt';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from "react-router-dom";
+import Login from "./components/Login/Login";
+import Signup from "./components/Signup/Signup";
+import Home from "./components/Home/Home";
+import Logout from "./components/Logout/Logout";
+import { getJWT } from "./utils/jwt";
 import AdminHome from "./components/Admin/AdminHome";
 import CategoryLayout from "./components/Admin/Categories/CategoryLayout";
 import ForgotPassword from "./components/ForgotPassword/ForgotPassword";
 import ResetPassword from "./components/ResetPassword/ResetPassword";
 
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/forgot_password" element={<ForgotPassword />} />
-        <Route path="/reset_password/:token" elsement={<ResetPassword />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="signup" element={<Signup />} />
-        <Route path="/logout" element={<Logout />} />
+	return (
+		<Router>
+			<Routes>
+				<Route path="/forgot_password" element={<ForgotPassword />} />
+				<Route path="/reset_password/:token" elsement={<ResetPassword />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="signup" element={<Signup />} />
+				<Route path="/logout" element={<Logout />} />
 
-        <Route path="/" element={<PrivateRoute>
-          <Home />
-        </PrivateRoute>}>
-        </Route>
+				<Route
+					path="/"
+					element={
+						<PrivateRoute>
+							<Home />
+						</PrivateRoute>
+					}
+				></Route>
 
-        <Route exact path="/admin" element={<PrivateRoute>
-          <AdminHome />
-        </PrivateRoute>}>
-        </Route>
+				<Route
+					exact
+					path="/admin"
+					element={
+						<PrivateRoute>
+							<AdminHome />
+						</PrivateRoute>
+					}
+				></Route>
 
-        <Route path="/admin/categories" element={<PrivateRoute>
-          <CategoryLayout />
-        </PrivateRoute>}></Route>
-
-      </Routes>
-    </Router>
-  );
-}
+				<Route
+					path="/admin/categories"
+					element={
+						<PrivateRoute>
+							<CategoryLayout />
+						</PrivateRoute>
+					}
+				></Route>
+			</Routes>
+		</Router>
+	);
+};
 
 function PrivateRoute({ children }) {
-  const auth = useAuth();
-  return auth ? children : <Navigate to="/login" />;
+	const auth = useAuth();
+	return auth ? children : <Navigate to="/login" />;
 }
 function useAuth() {
-  const jwt = getJWT();
-  return jwt && jwt !== 'undefined' && jwt !== 'null';
+	const jwt = getJWT();
+	return jwt && jwt !== "undefined" && jwt !== "null";
 }
 
 export default App;
