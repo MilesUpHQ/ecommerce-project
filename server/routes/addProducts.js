3bc8803cd9e47011105a4588cc4c61d6ff951dd5
AbhishekDeb70 <abhistf@gmail.com>
2022-02-01 18:02:18 +0530

    Working Image Field

--- server/routes/addProducts.js
+++ server/routes/addProducts.js
@@ -5,6 +5,7 @@ const multer  = require('multer');
 
   const fileStorageEngine = multer.diskStorage({
     destination:(req,file,cb)=>{
+      
       cb(null,"./images");//"/images" is  folder storage
     },
     filename: (req, file, cb)=>{
@@ -13,7 +14,7 @@ const multer  = require('multer');
   });
   const upload = multer({storage:fileStorageEngine});
 
-router.post("/",(req,res)=>{
+router.post("/",upload.single("file"),(req,res)=>{
     console.log("Request.body",req);
       //  table.integer('category_id').unsigned().notNullable();
     knex("products").insert({ name:req.body.name, price: req.body.price,description:req.body.description , category_id:null })
@@ -27,13 +28,13 @@ router.post("/",(req,res)=>{
     })
 })
 
-router.post("/single", upload.single("image"),  (req, res)=> {
+router.post("/single", upload.single("variant_images"),  (req, res)=> {
   console.log("Request.file",req.file);//display info on image file 
   res.send("Single File Upload Sucesss");
  });
 
 
- router.post("/multiple",upload.array("images",3),
+ router.post("/multiple",upload.array("variant_images",3),
  (req,res)=>{
    console.log(req.files);
    res.send("Multiple image upload sucess");
