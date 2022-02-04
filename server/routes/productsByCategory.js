5c69b8a777d545c7c0849bba9164cb3654d7950a
manisaiprasadam <manisaiprasadam@gmail.com>
2022-02-04 18:53:10 +0530

    created productsByCategory route

---
+++ server/routes/productsByCategory.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+const knex = require("../utils/dbConfig");
+
+router.get("/:id", async (req, res, next) => {
+	// get products by product category with variants
+	console.log("req.params.id", req.params.id);
+	knex("products")
+		.join("variants", "products.id", "variants.product_id")
+		.join("variant_images", "variants.id", "variant_images.variant_id")
+		.where("products.category_id", req.params.id)
+		.select(
+			"products.id",
+			"products.name",
+			"products.description",
+			"variants.color",
+			"variants.size",
+			"variants.type",
+			"variants.price",
+			"variant_images.image_url"
+		)
+		.then((row) => {
+			console.log("Row", row);
+			res.send(row);
+		})
+		.catch((err) => {
+			res.send("error in getting products");
+		});
+});
+
+module.exports = router;
