d98c248e2084b3beeb23cc46084511e830c9252c
chaitanya005 <muvvalachaitanya05@gmail.com>
2022-02-16 16:24:08 +0530

    Update: Filtering products by Price

--- client/src/components/ProductsByCategory/ProductsByCategory.js
+++ client/src/components/ProductsByCategory/ProductsByCategory.js
@@ -9,12 +9,29 @@ export default function ProductsByCategory() {
   const [errorMsg, setErrorMsg] = React.useState(null);
   const [searchItem, setSearchItem] = React.useState([]);
   const [options, setOptions] = React.useState([]);
+  const [minPrice, setMinPrice] = React.useState(null);
+  const [maxPrice, setMaxPrice] = React.useState(null);
+  const [range, setRange] = React.useState(null);
+  const [prices, setPrices] = React.useState([]);
   const categoryId = useParams().category;
-  console.log("categoryId", categoryId);
+  // console.log("categoryId", categoryId);
   useEffect(() => {
     const fetchData = async () => {
       try {
         const { data } = await axios.get(`/products/category/${categoryId}`);
+        let minPrice = Number.POSITIVE_INFINITY;
+        let maxPrice = Number.NEGATIVE_INFINITY;
+        for (let item of data) {
+          if (item.price < minPrice) minPrice = item.price;
+          if (item.price > maxPrice) maxPrice = item.price;
+        }
+        setMinPrice(minPrice);
+        setMaxPrice(maxPrice);
+        const closest = data.reduce((a, b) => {
+          return Math.abs(b.price - 2500) < Math.abs(a.price - 2500) ? b : a;
+        });
+        setRange(closest.price);
+        // prices.push(minPrice, closest.price, maxPrice)
         setProducts(data);
       } catch (error) {
         setErrorMsg("Sorry! Something went wrong. Please Try again", error);
@@ -27,7 +44,7 @@ export default function ProductsByCategory() {
     axios
       .get(`/typeahead-items?search_keyword=${value}`)
       .then((res) => {
-        console.log(res)
+        console.log(res);
         let array = res.data.map(({ id, name }) => ({
           label: name,
           value: id,
@@ -42,12 +59,43 @@ export default function ProductsByCategory() {
       axios
         .get(`/products-by-search?search_keyword=${searchItem[0].label}`)
         .then((res) => {
+          let minPrice = Number.POSITIVE_INFINITY;
+          let maxPrice = Number.NEGATIVE_INFINITY;
+          for (let item of res.data) {
+            if (item.price < minPrice) minPrice = item.price;
+            if (item.price > maxPrice) maxPrice = item.price;
+          }
+          setMinPrice(minPrice);
+          setMaxPrice(maxPrice);
+          const closest = res.data.reduce((a, b) => {
+            return Math.abs(b.price - 2500) < Math.abs(a.price - 2500) ? b : a;
+          });
+          setRange(closest.price);
           setProducts(res.data);
-          setSearchItem([])
+          // setSearchItem([]);
         })
         .catch((err) => console.log(err));
     }
   }, [searchItem]);
+
+  const handlePrice = (e, min, max) => {
+    if (e.target.checked) {
+      prices.push(min);
+      prices.push(max);
+    } else {
+      let indexOne = prices.indexOf(min);
+      prices.splice(indexOne, 1);
+      let indexTwo = prices.indexOf(max);
+      prices.splice(indexTwo, 1);
+    }
+
+    axios
+      .get(
+        `/filter-products/${categoryId}?prices_range=${JSON.stringify(prices)}`
+      )
+      .then((res) => setProducts(res.data))
+      .catch((err) => console.log(err));
+  };
 
   return (
     <>
@@ -56,11 +104,118 @@ export default function ProductsByCategory() {
         searchItem={searchItem}
         setSearchItem={setSearchItem}
         options={options}
-        placeholder={'Search for products'}
+        placeholder={"Search for products"}
       />
-      <div>
-        <h1>Products By Category</h1>
-        <div className="row">
+      {/* <h1>Products By Category</h1> */}
+
+      <br />
+      <div className="row">
+        <div className="col-4">
+          <aside className="ml-2">
+            <div className="card" style={{ display: "contents" }}>
+              {/* <article className="card-group-item">
+                <header className="card-header">
+                  <h6 className="title">Filters</h6>
+                </header>
+                <div className="filter-content">
+                  <div className="card-body">
+                    <label style={{ display: "flex", alignItems: "center" }}>
+                      <input
+                        type="radio"
+                        value="small"
+                        style={{
+                          width: "1rem",
+                          height: "1rem",
+                          marginRight: "0.3rem",
+                        }}
+                      />
+                      Men
+                    </label>
+                    <label style={{ display: "flex", alignItems: "center" }}>
+                      <input
+                        type="radio"
+                        value="small"
+                        style={{
+                          width: "1rem",
+                          height: "1rem",
+                          marginRight: "0.3rem",
+                        }}
+                      />
+                      Women
+                    </label>
+                    <label style={{ display: "flex", alignItems: "center" }}>
+                      <input
+                        type="radio"
+                        value="small"
+                        style={{
+                          width: "1rem",
+                          height: "1rem",
+                          marginRight: "0.3rem",
+                        }}
+                      />
+                      Kids
+                    </label>
+                  </div>
+                </div>
+              </article> */}
+              <article className="card-group-item">
+                <header className="card-header">
+                  <h6 className="title">Price Filter</h6>
+                </header>
+                <div class="filter-content">
+                  <div class="card-body">
+                    <div class="custom-control custom-checkbox">
+                      <input
+                        type="checkbox"
+                        class="custom-control-input"
+                        id="Check1"
+                        // value={`${minPrice} - ${range}`}
+                        // onChange={(e) =>
+                        //   e.target.checked
+                        //     ? handlePrice(e, minPrice, range)
+                        //     : (prices.pop(minPrice), prices.pop(range))
+                        // }
+                        onChange={(e) => handlePrice(e, minPrice, range)}
+                      />
+                      <label class="custom-control-label" for="Check1">
+                        {minPrice} - {range}
+                      </label>
+                    </div>
+
+                    <div class="custom-control custom-checkbox">
+                      <input
+                        type="checkbox"
+                        class="custom-control-input"
+                        id="Check2"
+                        // onChange={(e) =>
+                        //   e.target.checked
+                        //     ? handlePrice(range, maxPrice)
+                        //     : (prices.pop(range), prices.pop(maxPrice))
+                        // }
+                        onChange={(e) => handlePrice(e, range, maxPrice)}
+                      />
+                      <label class="custom-control-label" for="Check2">
+                        {range} - {maxPrice}
+                      </label>
+                    </div>
+
+                    {/* <div class="custom-control custom-checkbox">
+                      <input
+                        type="checkbox"
+                        class="custom-control-input"
+                        id="Check3"
+                      />
+                      <label class="custom-control-label" for="Check3">
+                        5000 & above
+                      </label>
+                    </div> */}
+                  </div>
+                </div>
+              </article>
+            </div>
+          </aside>
+        </div>
+        <div className="col-8">
           {errorMsg ? (
             <div className="alert alert-danger">{errorMsg}</div>
           ) : (
--- server/routes/searchProducts.js
+++ server/routes/searchProducts.js
@@ -54,4 +54,55 @@ router.get("/typeahead-items", async (req, res) => {
   }
 });
 
+router.get("/filter-products/:id", (req, res) => {
+  try {
+    let priceRange = JSON.parse(req.query.prices_range);
+    if (!req.query.prices_range || req.query.prices_range.length == 2) {
+      db("products as p")
+        .leftJoin("product_categories as c", "p.category_id", "c.id")
+        .leftJoin("variants", "variants.product_id", "p.id")
+        .select(
+          "p.id",
+          "p.category_id",
+          "p.name as name",
+          "p.description",
+          "c.name as category",
+          "variants.price"
+        )
+        .where("p.category_id", req.params.id)
+        .then((row) => {
+          res.json(row);
+        })
+        .catch((err) => {
+          res.send("error in getting products");
+        });
+    } else {
+      db("products")
+        .leftJoin("variants", "products.id", "variants.product_id")
+        .leftJoin("variant_images", "variants.id", "variant_images.variant_id")
+        .where("products.category_id", req.params.id)
+        .andWhere("variants.price", ">=", Math.min(...priceRange))
+        .andWhere("variants.price", "<=", Math.max(...priceRange))
+        .select(
+          "products.id",
+          "products.name",
+          "products.description",
+          "variants.color",
+          "variants.size",
+          "variants.type",
+          "variants.price",
+          "variant_images.image_url"
+        )
+        .then((row) => {
+          res.send(row);
+        })
+        .catch((err) => {
+          res.send("error in getting products");
+        });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 module.exports = router;
