c06fafd9aa2ff3f5204d906d093112c426f26da5
chaitanya005 <muvvalachaitanya05@gmail.com>
2022-02-23 20:35:35 +0530

    Update in knex file

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,28 +1,31 @@
 // Update with your config settings.
 require("dotenv").config();
-module.exports = {
-  development: {
-    client: process.env.DB_CLIENT,
-    connection: {
-      host: process.env.DB_HOST,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-    },
-  },
-  production: {
-    client: "pg",
-    connection: process.env.DATABASE_URL,
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-};
+
+const options =
+  process.env.NODE_ENV === "production"
+    ? {
+        client: "pg",
+        connection: process.env.DATABASE_URL,
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      }
+    : {
+        client: process.env.DB_CLIENT,
+        connection: {
+          host: process.env.DB_HOST,
+          database: process.env.DB_NAME,
+          user: process.env.DB_USER,
+          password: process.env.DB_PASS,
+        },
+        pool: {
+          min: 2,
+          max: 10,
+        },
+        migrations: {
+          tableName: "knex_migrations",
+        },
+      };
+
+module.exports = options;
