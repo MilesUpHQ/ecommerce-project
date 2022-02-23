953dde13f3bbc6231f46ef2f90c2e4ed0bcfc53c
chaitanya005 <muvvalachaitanya05@gmail.com>
2022-02-23 20:20:14 +0530

    UPdate in production settings

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,5 +1,5 @@
 // Update with your config settings.
-require('dotenv').config();
+require("dotenv").config();
 module.exports = {
   development: {
     client: process.env.DB_CLIENT,
@@ -15,6 +15,14 @@ module.exports = {
     },
     migrations: {
       tableName: "knex_migrations",
+    },
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
   },
 };
