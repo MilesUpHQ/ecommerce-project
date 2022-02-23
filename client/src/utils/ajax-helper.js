49cfcb1a76b2a06153922e110d6f2e902e6755e0
chaitanya005 <muvvalachaitanya05@gmail.com>
2022-02-23 20:26:28 +0530

    Update: Production url in ajax-helper.js

--- client/src/utils/ajax-helper.js
+++ client/src/utils/ajax-helper.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:4000/api/",
+  baseURL: "https://ecom-server1.herokuapp.com/api/",
 });
 
 export default instance;
