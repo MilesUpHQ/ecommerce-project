7d0455357f9381d11797a50382b707fdea8d0a8d
chaitanya005 <muvvalachaitanya05@gmail.com>
2022-02-23 21:29:45 +0530

    Update dbConfig file

--- server/utils/dbConfig.js
+++ server/utils/dbConfig.js
@@ -1,13 +1,5 @@
-const environment = process.env.NODE_ENV || "development";
-const config = require("../knexfile")[environment];
-const knex = require("knex")(config);
+const knex = require("knex");
+const config = require("../knexfile.js");
+const db = knex(config);
 
-module.exports = knex;
-
-// const knex = require("knex");
-
-// const config = require("../knexfile.js");
-
-// const db = knex(config);
-
-// module.exports = db;
+module.exports = db;
