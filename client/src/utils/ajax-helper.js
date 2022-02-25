dece69ff4686d9c67c3519a9bbd772cefe1a30bf
chaitanya005 <muvvalachaitanya05@gmail.com>
2022-02-25 11:53:24 +0530

    Update: .env variables in client, search bar styling, rm  erros in categoreis pages

--- client/src/utils/ajax-helper.js
+++ client/src/utils/ajax-helper.js
@@ -1,6 +1,10 @@
 import axios from "axios";
+
 const instance = axios.create({
-  baseURL: "https://ecom-server1.herokuapp.com/api/",
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? process.env.REACT_APP_PROD_BASE_URL + `/api/`
+      : process.env.REACT_APP_BASE_URL + `/api/`,
 });
 
 export default instance;
