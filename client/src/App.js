5681ee6881c2393929dfcc5f724dce75d508f6ac
Mani Sai Prasad <manisaiprasadam@gmail.com>
2022-03-04 17:11:58 +0530

    Restructuring routes and removed unused imports

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,6 @@ import {
   Routes,
   Route,
   Navigate,
-  Switch,
-  Link,
 } from "react-router-dom";
 import Login from "./components/Login/Login";
 import Signup from "./components/Signup/Signup";
@@ -22,7 +20,6 @@ import { ProductLayout } from "./components/Admin/Products/ProductLayout";
 import { ProductView } from "./components/Admin/Products/ProductView";
 import { User } from "./components/Admin/Products/User";
 import { UserView } from "./components/Admin/Products/UserView";
-import Navbar from "./components/Navbar/Navbar";
 import ProductsByCategory from "./components/ProductsByCategory/ProductsByCategory";
 import FeaturedProductsList from "./components/Home/ProductList";
 import FeaturedProductLayout from "./components/Admin/FeatureProducts/FeaturedProductLayout";
@@ -31,37 +28,20 @@ import Cart from "./components/Cart/Cart";
 import Address from "./components/Address/Address";
 import CreateAddress from "./components/Address/CreateAddress";
 import Checkout from "./components/CheckOut/Checkout";
-import Add from "./components/Cart/Add";
 import OrderConfirm from "./components/OrderConfirm/OrderConfirm";
 import OrderFailed from "./components/FailedError/FailedError";
 const App = () => {
   return (
     <Router>
       <Routes>
+        {/* Auth Routes */}
         <Route path="/forgot_password" element={<ForgotPassword />} />
         <Route path="/reset_password/:token" element={<ResetPassword />} />
         <Route path="/login" element={<Login />} />
         <Route path="signup" element={<Signup />} />
         <Route path="/logout" element={<Logout />} />
-        <Route path="/product/view/:id" element={<ViewProduct />} />
-        <Route
-          path="/admin/product/featured"
-          element={<FeaturedProductLayout />}
-        />
-        <Route path="/admin/products/add" element={<AddProducts />} />
-        <Route path="/admin/products" element={<ProductLayout />} />
-        <Route path="/" element={<FeaturedProducts />}></Route>
-        <Route path="/products/:category" element={<ProductsByCategory />} />
-        <Route path="/admin/products/:id/view" element={<ProductView />} />
-        <Route path="/admin/user/:id/view" element={<UserView />} />
-        <Route path="/admin/product/:id/update" element={<Edit />} />
-        <Route path="/admin/user" element={<User />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/user/address" element={<Address />} />
-        <Route path="/user/address/:id" element={<CreateAddress />} />
-        <Route path="/checkout/:id" element={<Checkout />} />
-        <Route path="/order/confirm" element={<OrderConfirm />} />
-        <Route path="/order/error" element={<OrderFailed />} />
+
+        {/* Admin Routes */}
 
         <Route
           exact
@@ -81,6 +61,28 @@ const App = () => {
             </PrivateRoute>
           }
         ></Route>
+
+        <Route
+          path="/admin/product/featured"
+          element={<FeaturedProductLayout />}
+        />
+        <Route path="/admin/products/add" element={<AddProducts />} />
+        <Route path="/admin/products" element={<ProductLayout />} />
+        <Route path="/admin/products/:id/view" element={<ProductView />} />
+        <Route path="/admin/user/:id/view" element={<UserView />} />
+        <Route path="/admin/product/:id/update" element={<Edit />} />
+        <Route path="/admin/user" element={<User />} />
+
+        {/* User Routes */}
+        <Route path="/" element={<FeaturedProducts />}></Route>
+        <Route path="/products/:category" element={<ProductsByCategory />} />
+        <Route path="/product/view/:id" element={<ViewProduct />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/user/address" element={<Address />} />
+        <Route path="/user/address/:id" element={<CreateAddress />} />
+        <Route path="/checkout/:id" element={<Checkout />} />
+        <Route path="/order/confirm" element={<OrderConfirm />} />
+        <Route path="/order/error" element={<OrderFailed />} />
       </Routes>
     </Router>
   );
