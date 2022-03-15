ceb36bff6ae28e4fb991a3693ad61dbfccce9f75
GangotriSasi <gangotri@sedintechonologies.com>
2022-03-15 10:57:22 +0530

    hd images

--- client/src/utils/imageData.js
+++ client/src/utils/imageData.js
@@ -1,12 +1,10 @@
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 let imageData = [
-  BASE_URL + "/data/image1.jpeg",
-  BASE_URL + "/data/image2.jpeg",
-  BASE_URL + "/data/image3.jpeg",
-  BASE_URL + "/data/image4.jpeg",
-  BASE_URL + "/data/image5.jpeg",
-  BASE_URL + "/data/image6.jpeg",
+  BASE_URL + "/data/image7.jpeg",
+  BASE_URL + "/data/image8.jpeg",
+  BASE_URL + "/data/image9.jpeg",
+  BASE_URL + "/data/image10.jpeg",
 ];
 
 export default imageData;
