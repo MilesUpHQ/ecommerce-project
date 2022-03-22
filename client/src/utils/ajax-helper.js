3c3ba455eff4ee2707add1669562adad2d46d5eb
Mani Sai Prasad <manisaiprasadam@gmail.com>
2022-03-22 17:25:08 +0530

    Added interceptors to handle 401 to redirect to login

--- client/src/utils/ajax-helper.js
+++ client/src/utils/ajax-helper.js
@@ -7,4 +7,24 @@ const instance = axios.create({
       : process.env.REACT_APP_BASE_URL + `/api/`,
 });
 
+instance.interceptors.response.use(
+  (res) => {
+    console.log("Valid response", res);
+    // Add configurations here
+    if (res.status === 401) {
+      console.log("401");
+      localStorage.removeItem("ecom_token");
+      window.location.href = "/login";
+    }
+    return res;
+  },
+  (err) => {
+    console.log("Invalid response", err);
+    if (err.response.status === 401) {
+      localStorage.removeItem("ecom_token");
+      window.location.href = "/login";
+    }
+    return JSON.stringify(err);
+  }
+);
 export default instance;
