a11ca17ac4e9604d4315269239bc203500bcc0a5
AbhishekDeb70 <abhistf@gmail.com>
2022-03-29 10:40:04 +0530

    small bug fixed

--- client/src/components/Admin/Products/DisplayProducts.js
+++ client/src/components/Admin/Products/DisplayProducts.js
@@ -53,9 +53,6 @@ const DisplayProducts = ({}) => {
         });
     }
   };
-  const toggleBtn = ()=>{
-    setReadMore(prevState => !prevState)
-  }
 
   return (
     <div className="main-panel">
