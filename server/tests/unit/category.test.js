310132da5739cc5509c22b481870d9b7e57e2f30
chaitanya005 <muvvalachaitanya05@gmail.com>
2022-04-06 23:09:31 +0530

    Add: test cases for category

---
+++ server/tests/unit/category.test.js
@@ -0,0 +1,28 @@
+const request = require("supertest");
+const { deleteCategory } = require("../../queries/category");
+const router = require("../../routes/admin/categories/category");
+const httpMocks = require("node-mocks-http");
+
+beforeEach(() => {
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+});
+let id = 1;
+describe("test category route", () => {
+  test("should return 200 status code on creating category", async () => {
+    request(router)
+      .post("/add")
+      .send({
+        categoryName: "New Cat",
+        searchItemId: 0,
+      })
+      .expect(200);
+  });
+
+  test('should return 200 status code on deleting category', async() => {
+      request(router)
+      .delete('/delete')
+      .query({ category_id: id }).expect(200)
+  })
+});
+
